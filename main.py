39a76f134d1a5bd8314236f604b59561f5ee25d5
tinashebutsa <tbutsa@gmail.com>
2024-09-06 13:35:47 +0200

    Initial login template with roles

--- main.py
+++ main.py
@@ -10,14 +10,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.is_json:
-        if request.method == "GET":
-            number = randint(1, 10)
-            return jsonify({'number': number})
-        if request.method == "POST":
-            text = float(request.data)
-            new_text = f"I got: {text}"
-            return jsonify({'data': new_text})
 
     return render_template('login.html')
 
