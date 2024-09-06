165b6fafa4c3ed2a04029057364eb1f8845d7ab2
tinashebutsa <tbutsa@gmail.com>
2024-09-06 13:24:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from random import randint
+from flask import (
+    Flask,
+    render_template, 
+    jsonify, 
+    request
+)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.is_json:
+        if request.method == "GET":
+            number = randint(1, 10)
+            return jsonify({'number': number})
+        if request.method == "POST":
+            text = float(request.data)
+            new_text = f"I got: {text}"
+            return jsonify({'data': new_text})
+
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run()
