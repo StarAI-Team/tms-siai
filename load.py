af1ddf6f04bb0a5537ef3db05cfdee7c1786725b
emelda-debug <teacayemmie@gmail.com>
2024-09-16 19:23:27 +0400

    changes made

--- load.py
+++ load.py
@@ -5,7 +5,11 @@ import requests
 import os
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+import uuid
+>>>>>>> dba0274ca74ed4eca7eaaf3570fe8dfe5def8f1a
 =======
 import uuid
 >>>>>>> dba0274ca74ed4eca7eaaf3570fe8dfe5def8f1a
