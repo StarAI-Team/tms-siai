476631a560c51a71005fc0dc3ef46e8d58f23b96
StarAI-Team <staraiinternational@gmail.com>
2024-09-19 17:52:03 +0200

    Latest Kafka login sequence dashboard transporter list

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import pandas as pd
 
 # Initialize the Flask application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///admin.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
