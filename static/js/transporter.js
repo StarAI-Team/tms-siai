9682e7f9689c3225a13206401a10a5384f4bec7d
emelda-debug <teacayemmie@gmail.com>
2024-10-08 18:58:31 +0400

    more shipper functions

--- static/js/transporter.js
+++ static/js/transporter.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const requiredFields = {
-        section1: ['first_name', 'last_name', 'phone_number', 'id_number','id_number', 'company_name', 'company_location', 'company_email'],
-        section2: ['company_contact', 'bank_name', 'account_name', 'account_number', 'directorship_text','directorship', 'proof_of_current_address_text', 'proof_of_current_address' ],
-        section3: ['tax_clearance_text', 'tax_clearance', 'tax_expiry', 'certificate_of_incorporation_text', 'certificate_of_incorporation','operators_licence_text', 'operators_licence', 'operators_expiry', 'permits_text', 'permits', 'permit_expiry', 'tracking_licence_text','tracking_licence', ],
-        section4: ['number_of_trucks', 'num_of_trucks_text', 'num_of_trucks', 'reg_books_text','reg_books', 'certificate_of_fitness_text', 'certificate_of_fitness'],
+        section1: ['first_name', 'last_name', 'phone_number', 'id_number', 'company_name', 'company_location', 'company_email'],
+        section2: ['company_contact', 'bank_name', 'account_name', 'account_number', 'directorship_text', 'directorship', 'proof_of_current_address_text', 'proof_of_current_address'],
+        section3: ['tax_clearance_text', 'tax_clearance', 'tax_expiry', 'certificate_of_incorporation_text', 'certificate_of_incorporation', 'operators_licence_text', 'operators_licence', 'operators_expiry', 'permits_text', 'permits', 'permit_expiry', 'tracking_licence_text', 'tracking_licence'],
+        section4: ['number_of_trucks', 'num_of_trucks_text', 'num_of_trucks', 'reg_books_text', 'reg_books', 'certificate_of_fitness_text', 'certificate_of_fitness'],
         section5: ['user_name', 'profile_picture', 'password', 'confirm_password']
     }
     const form = document.getElementById('multiStepForm');
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const prevButtons = document.querySelectorAll('.prev-button');
     const formSteps = document.querySelectorAll('.form-step');
     const showPasswordCheckbox = document.getElementById('show_password');
-  
     
     document.getElementById('visibility_button1').addEventListener('click', function() {
         toggleVisibility('password', 'icon1');
@@ -57,50 +56,19 @@ document.addEventListener("DOMContentLoaded", function() {
         inputs.forEach(input => {
             // Checking if the input is in the required list for the current step
             if (required.includes(input.name)) {
-                console.log("Validating input: ", input.name);
                 if (!input.checkValidity()) { 
                     isValid = false; 
                     input.classList.add('error'); 
                     showErrorMessage(`Please fill out the required field: ${input.placeholder}`); 
-                    console.log("Input " + input.name + " is invalid.");
-                    console.log("Validation message: ", input.validationMessage);
                 } else {
                     input.classList.remove('error'); 
-                    console.log("Input " + input.name + " is valid.");
                 }
             }
         });
         
         return isValid; 
     }
-    
-    document.addEventListener('DOMContentLoaded', () => {
-        // Function to update the text input with selected file names
-        function handleFileInput(event, textInputId) {
-            const input = event.target;
-            const textInput = document.getElementById(textInputId);
-            
-            // Get list of files
-            const files = input.files;
-            let fileNames = [];
-            
-            // Loop through the file list and collect names
-            for (let i = 0; i < files.length; i++) {
-                fileNames.push(files[i].name);
-            }
-            
-            // Join file names and display them in the text input
-            textInput.value = fileNames.join(', ');
-        }
-    
-        // Attach event listeners for file input changes
-        const regBooksInput = document.getElementById('reg_books');
-        const fitnessCertInput = document.getElementById('certificate_of_fitness');
-    
-        regBooksInput.addEventListener('change', (e) => handleFileInput(e, 'reg_books_text'));
-        fitnessCertInput.addEventListener('change', (e) => handleFileInput(e, 'certificate_of_fitness_text'));
-    });
-    
+
     function showErrorMessage(message) {
         const errorMessageDiv = document.querySelector('#step-error-message');
         errorMessageDiv.innerText = message;
@@ -116,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function() {
     async function sendEventData(section, shouldRedirect) {
         const formData = new FormData();
         const inputs = formSteps[currentStep].querySelectorAll('input, textarea');
-    
+
         // Collecting form data from inputs in the current section
         inputs.forEach(input => {
             if (input.name) {
@@ -132,48 +100,42 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             }
         });
-    
-    
-        console.log("Form data for section:", section, formData);
-    
+
         // Fetching user_id and ip_address metadata from the backend
         try {
             const metadataResponse = await fetch('/get_user_metadata');
             const metadata = await metadataResponse.json();
-    
+
             // Adding user metadata to FormData
-        formData.append('event_name', `transporterRegistration_${section}`);
-        formData.append('user_id', metadata.user_id);
-        formData.append('ip_address', metadata.ip_address);
-        formData.append('timestamp', new Date().toISOString());
-        formData.append('user_agent', navigator.userAgent);
-        formData.append('current_section', section);
-    
-        console.log("Payload to be sent:", Array.from(formData.entries()));;
-        // Sending the section data to the Flask backend
-        const response = await fetch('/register_transporter', {
-            method: 'POST',
-            body: formData,  // Use FormData for the body
-        });
+            formData.append('event_name', `transporterRegistration_${section}`);
+            formData.append('user_id', metadata.user_id);
+            formData.append('ip_address', metadata.ip_address);
+            formData.append('timestamp', new Date().toISOString());
+            formData.append('user_agent', navigator.userAgent);
+            formData.append('current_section', section);
 
-        const responseData = await response.json();
-        console.log('Response data:', responseData);
+            // Sending the section data to the Flask backend
+            const response = await fetch('/register_transporter', {
+                method: 'POST',
+                body: formData,  // Use FormData for the body
+            });
 
-        if (responseData.error) {
-            alert(`Error: ${responseData.error}`);
-            return false;  // Return false if there's an error
-        } else {
-            return true;  // Return true for a successful submission
+            const responseData = await response.json();
+
+            if (responseData.error) {
+                alert(`Error: ${responseData.error}`);
+                return false;  // Return false if there's an error
+            } else {
+                return true;  // Return true for a successful submission
+            }
+        } catch (error) {
+            console.error('Error sending data:', error);
+            alert('There was a problem submitting your data. Please try again.');
+            return false;  // Return false on catch
         }
-    } catch (error) {
-        console.error('Error sending data:', error);
-        alert('There was a problem submitting your data. Please try again.');
-        return false;  // Return false on catch
     }
-}
     
-    
-    // Attaching event listener
+    // Attaching event listener to next buttons
     nextButtons.forEach(button => {
         button.addEventListener("click", async function() {
             if (validateFormStep()) {  // Performing validation for the current step
@@ -188,23 +150,15 @@ document.addEventListener("DOMContentLoaded", function() {
                     } else {
                         // All sections completed successfully, validate password
                         const password = document.getElementById("password").value;
-                        const confirmPasswordElement = document.getElementById("confirm_password");
-                        
-                        if (!confirmPasswordElement) {
-                            console.error("Confirm password field is missing!");
-                            return; 
-                        }
-    
-                        const confirmPassword = confirmPasswordElement.value;
+                        const confirmPassword = document.getElementById("confirm_password").value;
     
                         // Validate password
                         if (password !== confirmPassword) {
-                            confirmPasswordElement.classList.add('error');
+                            document.getElementById("confirm_password").classList.add('error');
                             showErrorMessage("Passwords do not match.");
                             alert("Passwords do not match! Try Again");
                         } else {
-                            confirmPasswordElement.classList.remove('error');
-                            console.log("Passwords match.");
+                            document.getElementById("confirm_password").classList.remove('error');
                             // Redirect to the desired URL upon successful validation
                             window.location.href = '/transporter-package';
                         }
@@ -216,17 +170,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
     
-    
-
- prevButtons.forEach(button => {
-    button.addEventListener("click", function() {
-        if (currentStep > 0) {
-            currentStep--;
-            showStep(currentStep);
-        }
+    prevButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            if (currentStep > 0) {
+                currentStep--;
+                showStep(currentStep);
+            }
+        });
     });
-});
-
 
     // Function to handle file name update for file inputs
     function handleFileNameUpdate(fileInputId, textInputId) {
